ec2af66633e63b004aeaf03bff38f5c66687cced
Vrinda V <vrindav@DN0a1f256c.SUNet>
2018-11-13 23:55:27 -0800

    converting au to wav files for all genres

--- process_data.py
+++ process_data.py
@@ -13,7 +13,52 @@ def main():
 		if filename.endswith(".au"):
 			print(filename)
 			tfm.build('./genres/blues/'+filename, './genres/blues/'+filename[:-2]+'wav')
-	print(librosa.core.load('./genres/blues/blues.00000.wav', sr=None))
+	
+	for filename in os.listdir('./genres/classical'):
+		if filename.endswith(".au"):
+			print(filename)
+			tfm.build('./genres/classical/'+filename, './genres/classical/'+filename[:-2]+'wav')
+
+	for filename in os.listdir('./genres/country'):
+		if filename.endswith(".au"):
+			print(filename)
+			tfm.build('./genres/country/'+filename, './genres/country/'+filename[:-2]+'wav')
+
+	for filename in os.listdir('./genres/disco'):
+		if filename.endswith(".au"):
+			print(filename)
+			tfm.build('./genres/disco/'+filename, './genres/disco/'+filename[:-2]+'wav')
+
+	for filename in os.listdir('./genres/hiphop'):
+		if filename.endswith(".au"):
+			print(filename)
+			tfm.build('./genres/hiphop/'+filename, './genres/hiphop/'+filename[:-2]+'wav')
+
+	for filename in os.listdir('./genres/jazz'):
+		if filename.endswith(".au"):
+			print(filename)
+			tfm.build('./genres/jazz/'+filename, './genres/jazz/'+filename[:-2]+'wav')
+
+	for filename in os.listdir('./genres/metal'):
+		if filename.endswith(".au"):
+			print(filename)
+			tfm.build('./genres/metal/'+filename, './genres/metal/'+filename[:-2]+'wav')
+
+	for filename in os.listdir('./genres/pop'):
+		if filename.endswith(".au"):
+			print(filename)
+			tfm.build('./genres/pop/'+filename, './genres/pop/'+filename[:-2]+'wav')
+
+	for filename in os.listdir('./genres/reggae'):
+		if filename.endswith(".au"):
+			print(filename)
+			tfm.build('./genres/reggae/'+filename, './genres/reggae/'+filename[:-2]+'wav')
+
+	for filename in os.listdir('./genres/rock'):
+		if filename.endswith(".au"):
+			print(filename)
+			tfm.build('./genres/rock/'+filename, './genres/rock/'+filename[:-2]+'wav')
+
 	print('Conversion done')
 
 if __name__ == '__main__':
