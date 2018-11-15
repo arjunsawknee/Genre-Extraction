ac04fa690af2ed27ae459d5ac6953cbcae4db998
Vrinda V <vrindav@DN51uk2q.SUNet>
2018-11-15 00:24:12 -0800

    Updated process_data to create csv files for songs and labels, created initial version of softmax

--- process_data.py
+++ process_data.py
@@ -2,62 +2,106 @@ import os
 import sox
 import numpy as np
 import librosa
+import pandas as pd
 
 def main():
 	"""
 		Converts all files of the GTZAN dataset
 		to the WAV (uncompressed) format.
 	"""
+#	df2 = pd.read_csv('songs.csv')
+#	print(df2.shape)
+#	exit()
+
 	tfm = sox.Transformer()
+	songs = np.zeros((1000, 40000))
+	labels = np.zeros((1000, 1))
+	counter = 0
+
 	for filename in os.listdir('./genres/blues'):
 		if filename.endswith(".au"):
-			print(filename)
 			tfm.build('./genres/blues/'+filename, './genres/blues/'+filename[:-2]+'wav')
+			audio, sr = librosa.core.load('./genres/blues/'+filename[:-2]+'wav')
+			songs[counter] = audio[:40000]
+			labels[counter] = 0
+			counter += 1
 	
 	for filename in os.listdir('./genres/classical'):
 		if filename.endswith(".au"):
-			print(filename)
 			tfm.build('./genres/classical/'+filename, './genres/classical/'+filename[:-2]+'wav')
+			audio, sr = librosa.core.load('./genres/classical/'+filename[:-2]+'wav')
+			songs[counter] = audio[:40000]
+			labels[counter] = 1
+			counter += 1
 
 	for filename in os.listdir('./genres/country'):
 		if filename.endswith(".au"):
-			print(filename)
 			tfm.build('./genres/country/'+filename, './genres/country/'+filename[:-2]+'wav')
+			audio, sr = librosa.core.load('./genres/country/'+filename[:-2]+'wav')
+			songs[counter] = audio[:40000]
+			labels[counter] = 2
+			counter += 1
 
 	for filename in os.listdir('./genres/disco'):
 		if filename.endswith(".au"):
-			print(filename)
 			tfm.build('./genres/disco/'+filename, './genres/disco/'+filename[:-2]+'wav')
+			audio, sr = librosa.core.load('./genres/disco/'+filename[:-2]+'wav')
+			songs[counter] = audio[:40000]
+			labels[counter] = 3
+			counter += 1
 
 	for filename in os.listdir('./genres/hiphop'):
 		if filename.endswith(".au"):
-			print(filename)
 			tfm.build('./genres/hiphop/'+filename, './genres/hiphop/'+filename[:-2]+'wav')
+			audio, sr = librosa.core.load('./genres/hiphop/'+filename[:-2]+'wav')
+			songs[counter] = audio[:40000]
+			labels[counter] = 4
+			counter += 1
 
 	for filename in os.listdir('./genres/jazz'):
 		if filename.endswith(".au"):
-			print(filename)
 			tfm.build('./genres/jazz/'+filename, './genres/jazz/'+filename[:-2]+'wav')
+			audio, sr = librosa.core.load('./genres/jazz/'+filename[:-2]+'wav')
+			songs[counter] = audio[:40000]
+			labels[counter] = 5
+			counter += 1
 
 	for filename in os.listdir('./genres/metal'):
 		if filename.endswith(".au"):
-			print(filename)
 			tfm.build('./genres/metal/'+filename, './genres/metal/'+filename[:-2]+'wav')
+			audio, sr = librosa.core.load('./genres/metal/'+filename[:-2]+'wav')
+			songs[counter] = audio[:40000]
+			labels[counter] = 6
+			counter += 1
 
 	for filename in os.listdir('./genres/pop'):
 		if filename.endswith(".au"):
-			print(filename)
 			tfm.build('./genres/pop/'+filename, './genres/pop/'+filename[:-2]+'wav')
+			audio, sr = librosa.core.load('./genres/pop/'+filename[:-2]+'wav')
+			songs[counter] = audio[:40000]
+			labels[counter] = 7
+			counter += 1
 
 	for filename in os.listdir('./genres/reggae'):
 		if filename.endswith(".au"):
-			print(filename)
 			tfm.build('./genres/reggae/'+filename, './genres/reggae/'+filename[:-2]+'wav')
+			audio, sr = librosa.core.load('./genres/reggae/'+filename[:-2]+'wav')
+			songs[counter] = audio[:40000]
+			labels[counter] = 8
+			counter += 1
 
 	for filename in os.listdir('./genres/rock'):
 		if filename.endswith(".au"):
-			print(filename)
 			tfm.build('./genres/rock/'+filename, './genres/rock/'+filename[:-2]+'wav')
+			audio, sr = librosa.core.load('./genres/rock/'+filename[:-2]+'wav')
+			songs[counter] = audio[:40000]
+			labels[counter] = 9
+			counter += 1
+
+	songs = pd.DataFrame(songs)
+	labels = pd.DataFrame(labels)
+	songs.to_csv('songs.csv', index = False)
+	labels.to_csv('labels.csv', index = False)
 
 	print('Conversion done')
 
---
+++ softmax.py
@@ -0,0 +1,62 @@
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import math
+
+
+def initialize_parameters(vector_dim = 40000, num_classes = 10):
+	w = np.random.randn(num_classes, vector_dim)
+	b = np.zeros((num_classes, 1))
+	return w, b
+
+def softmax(logits):
+    return np.exp(logits) / np.sum(np.exp(logits), axis=0)
+
+def forward_propagate(inputs, weights, bias):
+	score = np.matmul(weights, inputs) + bias
+	y_hat = softmax(score)
+	pred = np.argmax(y_hat)
+	return y_hat, pred
+
+def get_loss(y_hat, y):
+	return -1*(np.sum(y*np.log(y_hat)))
+
+def back_propagate(weights, bias, y_hat, y, inputs, learning_rate = 0.01):
+	db = y_hat-y
+	dw = db.dot(inputs.T)
+	new_weights = weights - learning_rate*dw
+	new_bias = bias - learning_rate*db
+	return new_weights, new_bias
+
+
+def train(songs, labels):
+	w, b = initialize_parameters()
+	for i in range(1000):
+		smoothed_cost_list = []
+		correct_class = 0
+		attempts = 0
+		for j, row in songs.iterrows():
+			label = labels.iloc[j]
+			label = label[0]
+			#print(type(row.values))
+			inputs = row.values
+			inputs = inputs.reshape((40000, 1))
+			y_hat, pred = forward_propagate(inputs, w, b)
+			if pred == label:
+				correct_class += 1
+			attempts += 1
+			curr_loss = get_loss(y_hat, label)
+			smoothed_cost_list.append(curr_loss)
+			w, b = back_propagate(w, b, y_hat, label, inputs)
+		smoothed_cost = float(sum(smoothed_cost_list))/len(smoothed_cost_list)
+		print("Epoch " + str(i) + ": Accuracy = " + str(float(correct_class)*100/attempts) + ", Smoothed Cost : "  + str(smoothed_cost))
+
+def main():
+	songs = pd.read_csv('songs.csv')
+	print("Loaded songs csv")
+	labels = pd.read_csv('labels.csv')
+	print("Loaded labels csv")
+	train(songs, labels)
+  
+if __name__== "__main__":
+	main()
