24cc4c946cfe2ca474cd7f07d9ca963536271485
Woodrow Wang <woody@DN0a1f211c.SUNet>
2018-12-08 12:37:02 -0800

    Changes

--- softmaxautoencoder.py
+++ softmaxautoencoder.py
@@ -7,12 +7,13 @@ import os
 import sox
 import librosa
 
-input_dim = 40000
+#input_dim = 40000
+input_dim = 500
 num_classes = 10
 alpha = 0.0001
 num_epochs = 200
-batch_size = 16
-classificationweight = 0.000001
+batch_size = 512
+classificationweight = 0.0001
 
 def get_one_hot(label_num, num_classes = 10):
     one_hot = np.zeros((1,num_classes))
@@ -27,19 +28,23 @@ def load_data():
 
 	print('Reading data...')
 	tfm = sox.Transformer()
-	songs = np.zeros((10000, 40000))
-	onehotlabels = np.zeros((10000, 10))
+	songs = np.zeros((20000, input_dim))
+	onehotlabels = np.zeros((20000, num_classes))
 	counter = 0
 
 	allgenres = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
 	# Splits each song into 10 examples of shape (40000, 1) ~ 2 seconds each
-	numsplit = 10
-	sizesplit = 40000
+	'''numsplit = 10
+	sizesplit = 40000'''
+
+	# splits of 1 second each:
+	numsplit = 20
+	sizesplit = input_dim
 	# generalized loop to process data
 	for index in range(len(allgenres)):
 		for filename in os.listdir('./genres/' + allgenres[index]):
 			if filename.endswith(".wav"):
-				audio, sr = librosa.core.load('./genres/' + allgenres[index] + '/' + filename)
+				audio, sr = librosa.core.load('./genres/' + allgenres[index] + '/' + filename, sr=500)
 				for j in range(numsplit):
 					songs[counter] = audio[(sizesplit * j) : (sizesplit * (j + 1))]
 					onehotlabels[counter] = get_one_hot(index)
@@ -49,17 +54,11 @@ def load_data():
 	print('Data reading done :)')
 	return songs, onehotlabels
 
-
-'''def load_data(songs_path, labels_path):
-	print("Loading data...")
-	songs = pd.read_csv(songs_path)
-	labels = pd.read_csv(labels_path)
-	print("Congrats mate.")
-	return songs, labels'''
-
 def get_placeholders():
 	inputs_placeholder = tf.placeholder(tf.float32, (None, input_dim))
 	labels_placeholder = tf.placeholder(tf.float32, (None, num_classes))
+	tf.add_to_collection('inputs_placeholder', inputs_placeholder)
+	tf.add_to_collection('labels_placeholder', labels_placeholder)
 	return inputs_placeholder, labels_placeholder
 
 def add_parameters():
@@ -100,32 +99,41 @@ def get_batches(seq, size=batch_size):
     return [seq[pos:pos + size] for pos in range(0, len(seq), size)]
 
 def train(X, Y, X_dev, Y_dev):
+	tf.reset_default_graph()
 	inputs_batch, labels_batch = get_placeholders()
 	weights = add_parameters()
 	encoding = encoder(inputs_batch, weights)
 	decoding = decoder(encoding, weights)
+	tf.add_to_collection("encoding", encoding)
+	tf.add_to_collection("decoding", decoding)
 
 	y_hat = softmaxclassifier(encoding, weights)
+	tf.add_to_collection("y_hat", y_hat)
 
 	# Check shape of labels: need to be shape (batch_size, num_classes) according to documentation
 	loss = tf.reduce_mean(tf.pow(decoding - inputs_batch, 2)) + tf.reduce_mean((classificationweight * tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels_batch, logits=y_hat)))
+	tf.add_to_collection("loss", loss)
 	# Just reconstruction loss: order of magnitude 0.005-0.01
 	#loss = tf.reduce_mean(tf.pow(decoding - inputs_batch, 2))
 	# Just cross entropy loss: order of magnitude 1.5 - 2.5
 	#loss = (classificationweight * tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels_batch, logits=y_hat))
 	optimizer = tf.train.AdamOptimizer(learning_rate = alpha).minimize(loss)
 	init = tf.global_variables_initializer()
+	saver = tf.train.Saver(max_to_keep=5)
 	with tf.Session() as sess:
+		summary_writer = tf.summary.FileWriter('./tensorboardlogs/softmaxautoencoder', sess.graph)
 		sess.run(init)
 		# Shuffling training set
 		ind_list=[i for i in range(X.shape[0])]
 		shuffle(ind_list)
 		X = X.iloc[ind_list]
 		Y = Y.iloc[ind_list]
+
+		train_accuracies = []
+		dev_accuracies = []
+		loss_per_epoch = []
 		for iteration in range(num_epochs):
 			inputs_batches = get_batches(X)
-			print(inputs_batches)
-
 			labels_batches = get_batches(Y)
 
 			cost_list = []
@@ -147,7 +155,7 @@ def train(X, Y, X_dev, Y_dev):
 
 				#_, _, preds, total_loss = sess.run([encoding, decoding, y_hat, loss], feed_dict={inputs_batch : X, labels_batch : Y})
 				#print ("Epoch " + str(iteration+1) + ", Update Number " + str(i)+ ", Summed Cost : "  + str(total_loss))
-				#cost_list.append(curr_loss)
+				cost_list.append(curr_loss)
 				#print(curr_loss)
 			accuracy = currnumcorrect / float(X.shape[0])
 			print("Epoch " + str(iteration+1) + ", Train Accuracy: " + str(accuracy))
@@ -158,9 +166,24 @@ def train(X, Y, X_dev, Y_dev):
 			numcorrect = tf.math.count_nonzero(numequal)
 			devaccuracy = numcorrect.eval() / float(X_dev.shape[0])
 			print("Epoch " + str(iteration+1) + ", Dev Accuracy: " + str(devaccuracy))
+			train_accuracies.append(accuracy)
+			dev_accuracies.append(devaccuracy)
+			train_smoothed_cost = float(sum(cost_list)) / len(cost_list)
+			loss_per_epoch.append(train_smoothed_cost)
+			print("Lists for plotting: ")
+			print("Train accuracies: ", train_accuracies)
+			print("Dev accuracies: ", dev_accuracies)
+			print("Train smoothed cost: ", loss_per_epoch)
+
+			saver.save(sess, './modelWeights/softmaxautoencoder', global_step = (iteration+1))
+			objectives_summary = tf.Summary()
+			objectives_summary.value.add(tag='train_accuracy', simple_value=accuracy)
+			objectives_summary.value.add(tag='dev_accuracy', simple_value=devaccuracy)
+			objectives_summary.value.add(tag='train_smoothed_cost', simple_value=train_smoothed_cost)
+			summary_writer.add_summary(objectives_summary, iteration+1)
+			summary_writer.flush()
 
 def main():
-	#songs, labels = load_data('songs.csv','onehotlabels.csv')
 	songs, labels = load_data()
 
 	# Shuffling training set
@@ -168,10 +191,15 @@ def main():
 	shuffle(ind_list)
 	songs = songs.iloc[ind_list]
 	labels = labels.iloc[ind_list]
-	songs_train = songs.iloc[0:8000]
-	songs_dev = songs.iloc[8000:]
-	labels_train = labels.iloc[0:8000]
-	labels_dev = labels.iloc[8000:]
+	songs_train = songs.iloc[0:18000]
+	songs_dev = songs.iloc[18000:]
+	labels_train = labels.iloc[0:18000]
+	labels_dev = labels.iloc[18000:]
+
+	# Write dev values to a csv for testing:
+	songs_dev.to_csv('songs_dev.csv', index = False)
+	labels_dev.to_csv('labels_dev.csv', index = False)
+
 	train(songs_train, labels_train, songs_dev, labels_dev)
 
 if __name__== "__main__":
