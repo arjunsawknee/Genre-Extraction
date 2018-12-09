d68f0ddfe416b8ba765000430ffedcb2bb46fec9
Vrinda V <vrindav@DN0a1f269e.SUNet>
2018-12-08 17:35:54 -0800

    Implemented PCA visualization in 2D and 3D

--- testsoftmaxautoencoder.py
+++ testsoftmaxautoencoder.py
@@ -8,7 +8,9 @@ import sox
 import librosa
 from softmaxautoencoder import load_data, get_one_hot
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.metrics import confusion_matrix
+from sklearn import decomposition
 import itertools
 
 
@@ -21,42 +23,104 @@ classificationweight = 0.0001
 
 # Borrowed from scikit-learn implementation of plotting the confusion matrix
 def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
+						  normalize=False,
+						  title='Confusion matrix',
+						  cmap=plt.cm.Blues):
+	"""
+	This function prints and plots the confusion matrix.
+	Normalization can be applied by setting `normalize=True`.
+	"""
+	if normalize:
+		cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+		print("Normalized confusion matrix")
+	else:
+		print('Confusion matrix, without normalization')
 
-    print(cm)
+	print(cm)
 
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    classes = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
+	plt.imshow(cm, interpolation='nearest', cmap=cmap)
+	plt.title(title)
+	plt.colorbar()
+	tick_marks = np.arange(len(classes))
+	#classes = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
+	classes = ['classical', 'jazz', 'metal', 'pop']
 
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+	plt.xticks(tick_marks, classes, rotation=45)
+	plt.yticks(tick_marks, classes)
+
+	fmt = '.2f' if normalize else 'd'
+	thresh = cm.max() / 2.
+	for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+		plt.text(j, i, format(cm[i, j], fmt),
+				 horizontalalignment="center",
+				 color="white" if cm[i, j] > thresh else "black")
+	plt.tight_layout()
+	plt.ylabel('True label')
+	plt.xlabel('Predicted label')
+
+def runPCA2D(sess, encoding, inputs_batch, labels_batch, X, Y):
+	allgenres = ['classical', 'jazz', 'metal', 'pop']
+
+	latents = sess.run([encoding], feed_dict={inputs_batch : X, labels_batch : Y})
+	print(Y.shape)
+	truelabels = tf.math.argmax(Y, axis=1).eval()
+
+	fig = plt.figure(1, figsize=(4, 3))
+	plt.clf()
+	ax = plt.axes()
+	plt.cla()
+
+	pca = decomposition.PCA(n_components = 3)
+	latents = latents[0]
+	pca.fit(latents)
+	latents = pca.transform(latents)
+
+	for name, label in [('Classical', 0), ('Jazz', 1), ('Metal', 2), ('Pop', 3)]:
+		ax.text(
+			latents[truelabels == label, 0].mean(),
+			latents[truelabels == label, 1].mean(),
+			name,
+			horizontalalignment = 'center',
+			bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
+
+	ax.scatter(latents[:, 0], latents[:, 1], c=truelabels, cmap=plt.cm.nipy_spectral,
+		   edgecolor='k')
+	plt.show()
+
+def runPCA3D(sess, encoding, inputs_batch, labels_batch, X, Y):
+	allgenres = ['classical', 'jazz', 'metal', 'pop']
+
+	latents = sess.run([encoding], feed_dict={inputs_batch : X, labels_batch : Y})
+	print(Y.shape)
+	truelabels = tf.math.argmax(Y, axis=1).eval()
+
+	fig = plt.figure(1, figsize=(4, 3))
+	plt.clf()
+	ax = plt.axes(projection = '3d')
+	plt.cla()
+
+	pca = decomposition.PCA(n_components = 3)
+	latents = latents[0]
+	pca.fit(latents)
+	latents = pca.transform(latents)
+
+	for name, label in [('Classical', 0), ('Jazz', 1), ('Metal', 2), ('Pop', 3)]:
+		ax.text(
+			latents[truelabels == label, 0].mean(),
+			latents[truelabels == label, 1].mean(),
+			latents[truelabels == label, 2].mean(),
+			name,
+			horizontalalignment = 'center',
+			bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
+
+	ax.scatter(latents[:, 0], latents[:, 1], latents[:, 2], c=truelabels, cmap=plt.cm.nipy_spectral,
+		   edgecolor='k')
+	plt.show()
+
 
 def test(X, Y):
 	with tf.Session() as sess:
-		new_saver = tf.train.import_meta_graph('./modelWeights/softmaxautoencoder-120.meta')
+		new_saver = tf.train.import_meta_graph('./modelWeights/softmaxautoencoder-200.meta')
 		new_saver.restore(sess, tf.train.latest_checkpoint('./modelWeights'))
 		graph = tf.get_default_graph()
 		inputs_batch = tf.get_collection('inputs_placeholder')[0]
@@ -75,7 +139,8 @@ def test(X, Y):
 
 		#Computes confusion matrix
 		#class_names = ["1 star", "2 star", "3 star", "4 star", "5 star"]
-		class_names = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+		#class_names = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+		class_names = [0, 1, 2, 3]
 		pred = predictions.eval()
 		correct_labels = truelabels.eval()
 		confmat = confusion_matrix(correct_labels, pred, class_names)
@@ -85,7 +150,12 @@ def test(X, Y):
 		plt.figure()
 		plot_confusion_matrix(confmat, classes=class_names, normalize=True, title='Normalized confusion matrix')
 		# Commented out to prevent showing the matrix everytime code is run
-		plt.show()
+		#plt.show()
+
+
+		runPCA2D(sess, encoding, inputs_batch, labels_batch, X, Y)
+		runPCA3D(sess, encoding, inputs_batch, labels_batch, X, Y)
+
 
 def main():
 	songs = pd.read_csv('songs_dev.csv')
